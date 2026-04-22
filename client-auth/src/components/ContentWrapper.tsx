c0f5507b71038e4750aee9c2f36e0e062184f4f3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-22 21:54:40 +0300

    fix: ContentWrapper

--- client-auth/src/components/ContentWrapper.tsx
+++ client-auth/src/components/ContentWrapper.tsx
@@ -6,7 +6,7 @@ import type { PropsWithChildren } from 'react';
 
 export default function ContentWrapper({ children }: PropsWithChildren) {
   return (
-    <main>
+    <main id="center">
       {/* <Header /> */}
       {children}
       {/* <Footer /> */}
