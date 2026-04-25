dd877d0dfd220e49f6bd07a5a22301b4dcf3bc5d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-25 12:12:42 +0300

    add: sidebar

---
+++ client-auth/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>sidebar</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </div>
+  );
+}
