a8bdb0841de37d429b122da592aeb62047d17651
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-25 21:10:18 +0300

    add: UpdateUserDto

--- core/src/users/dto/update-user.dto.ts
+++ core/src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
+  @IsOptional()
+  status: string;
+}
