a5ff6d08f00758b42879c7ae5b389a8162568f36
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-26 23:12:17 +0300

    fix: User

--- core/src/users/entities/user.entity.ts
+++ core/src/users/entities/user.entity.ts
@@ -14,8 +14,11 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar', length: 255, nullable: true })
   status: string;
 
-  // @OneToMany(() => Note, (note) => note.creator)
-  // notes: Note[];
+  @OneToMany(() => Note, (note) => note.creator)
+  notes: Note[];
+
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: true })
+  refreshToken: string;
 
   // @Column({ type: 'boolean', default: false, nullable: false })
   // isDark: boolean;
