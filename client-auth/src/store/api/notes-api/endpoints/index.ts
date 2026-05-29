04c92600710eb2f72d62a1fabb68ba39a151f840
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-29 09:44:09 +0300

    fix: notesApiEndpoints

---
+++ client-auth/src/store/api/notes-api/endpoints/index.ts
@@ -0,0 +1,55 @@
+import notesApi from '..';
+
+type Note = {
+  // id: number;
+  title: string;
+  preview: string;
+  content: string;
+  type: {
+    id: number;
+  };
+};
+
+type NoteResponse = {
+  id: number;
+  title: string;
+  content: string;
+  type: {
+    id: number;
+    name: string;
+  };
+};
+
+const notesApiEndpoints = notesApi
+  .enhanceEndpoints({
+    addTagTypes: ['Notes'],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      createNote: builder.mutation<NoteResponse, Note>({
+        query: (data: Note) => ({
+          url: '/notes',
+          method: 'POST',
+          body: data,
+        }),
+        invalidatesTags: ['Notes'],
+      }),
+      getNoteById: builder.query<NoteResponse, number>({
+        query: (id) => ({
+          url: `/notes/${id}`,
+          method: 'GET',
+        }),
+        providesTags: ['Notes'],
+      }),
+      getNotesByPage: builder.mutation<{ data: NoteResponse[]; total: number }, number>({
+        query: (page) => ({
+          url: `/notes/pages/${page}`,
+          method: 'GET',
+        }),
+        invalidatesTags: ['Notes'],
+      }),
+    }),
+  });
+
+export const { useCreateNoteMutation, useGetNoteByIdQuery, useGetNotesByPageMutation } = notesApiEndpoints;
+export { notesApiEndpoints };
