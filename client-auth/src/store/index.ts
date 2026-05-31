1f74b45433f825676ac181af56484e3bc01762e1
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-31 22:29:58 +0300

    fix: store

--- client-auth/src/store/index.ts
+++ client-auth/src/store/index.ts
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { authApi, usersApi, notesApi } from './api/index';
+import { authApi, usersApi, notesApi, typesApi } from './api/index';
 
 import authReducer from './slices/auth-slice';
 import usersReducer from './slices/users-slice';
 import themeReducer from './slices/theme-slice';
 import notesReducer from './slices/notes-slice';
+import typesReducer from './slices/types-slice';
 
 export * from './api/auth-api/endpoints/index';
 export * from './api/users-api/endpoints/index';
 export * from './api/notes-api/endpoints/index';
+export * from './api/types-api/endpoints/index';
 
 export * from './slices/index';
 
@@ -19,15 +21,18 @@ export const store = configureStore({
     users: usersReducer,
     theme: themeReducer,
     notes: notesReducer,
+    types: typesReducer,
     [authApi.reducerPath]: authApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [notesApi.reducerPath]: notesApi.reducer,
+    [typesApi.reducerPath]: typesApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(
       authApi.middleware,
       usersApi.middleware,
       notesApi.middleware,
+      typesApi.middleware,
     ),
 });
 
