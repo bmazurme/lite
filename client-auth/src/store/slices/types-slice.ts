9b2a92d409175e33724a2dc9a5f1a083f22bf07c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-31 12:09:01 +0300

    fix: typesSlice

---
+++ client-auth/src/store/slices/types-slice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { RootState, Type } from '..';
+
+type TypesState = {
+  types: Type[];
+};
+
+export const initialStateTypes: TypesState = {
+  types: [],
+};
+
+const typesSlice = createSlice({
+  name: 'types',
+  initialState: initialStateTypes,
+  reducers: {
+    setTypes: (state, action) => {
+      state.types = action.payload;
+    },
+  },
+});
+
+export const { setTypes } = typesSlice.actions;
+export default typesSlice.reducer;
+export const typesSelector = (state: RootState) => state.types.types;
